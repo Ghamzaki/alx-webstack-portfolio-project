2230bdf84c460e3b7bf4a3e6c7a07b8d6d3b844d
Ghamzaki <Uthmanghamzaki@gmail.com>
2024-11-09 08:55:59 +0000

    Database Management setup

---
+++ quiz-app/firebase_setup.py
@@ -0,0 +1,15 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate('./interactive-quiz-applica-ff179-firebase-adminsdk-15y82-158895a097.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+def store_result(user_id, score, total):
+    data = {
+        'user_id': user_id,
+        'score': score,
+        'total': total
+    }
+    db.collection('quiz_results').add(data)
